24c2489f47347fd154ca7993305de205177e9a51
Brenoqn2 <brenoqn2@gmail.com>
2022-05-15 04:07:15 -0300

    fix: console warnings

---
+++ src/components/productPage/productPage.jsx
@@ -0,0 +1,10 @@
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Header from "../publicComponents/Header";
+import Footer from "../publicComponents/Footer";
+import { ProductsContext } from "../../contexts/ProductsContext";
+
+export default function productPage() {
+  return;
+}
--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -1,18 +1,21 @@
 import { createContext, useState, useContext } from "react";
 
-export const ProductContext = createContext()
+export const ProductContext = createContext();
 
-export default function ProductsProvider({ children }){
-    const [products, setProducts] = useState([])
-    
-    return(
-        <ProductContext.Provider value={{products, setProducts}}>
-            {children}
-        </ProductContext.Provider>
-    )
+export default function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState("");
+
+  return (
+    <ProductContext.Provider
+      value={{ products, setProducts, selectedProduct, setSelectedProduct }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
 }
-export function useProducts(){
-    const context = useContext(ProductContext);
-    const { products, setProducts } = context;
-    return { products, setProducts }
+export function useProducts() {
+  const context = useContext(ProductContext);
+  const { products, setProducts } = context;
+  return { products, setProducts };
 }
