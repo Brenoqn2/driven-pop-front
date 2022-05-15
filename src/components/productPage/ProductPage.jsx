3431eea2e515f3d45584b4e35d3edec4dffb2f71
Brenoqn2 <brenoqn2@gmail.com>
2022-05-15 17:07:21 -0300

    changing product quantity + buy button

--- src/components/productPage/ProductPage.jsx
+++ src/components/productPage/ProductPage.jsx
@@ -8,6 +8,7 @@ import Footer from "../publicComponents/Footer";
 export default function ProductPage() {
   const [product, setProduct] = useState([]);
   const { productHandle } = useParams();
+  const [quantity, setQuantity] = useState(1);
   useEffect(() => {
     const URL = `http://localhost:5000/products/${productHandle}`;
     console.log(URL);
@@ -25,9 +26,35 @@ export default function ProductPage() {
       <ProductPageMain>
         {product.length > 0 ? (
           <>
-            <img src={product[0].imageName.replace("www", "images")} />
+            <img
+              src={product[0].imageName.replace("www", "images")}
+              alt={product[0].title}
+            />
             <div className="division"></div>
-            <h1>{product[0].title}</h1>
+            <ProductInfo>
+              <h1>{product[0].title}</h1>
+              <p>R$20,00</p>
+              <div>
+                <QuantityButton>
+                  <ion-icon
+                    name="remove-outline"
+                    onClick={() => {
+                      if (quantity > 1) {
+                        setQuantity(quantity - 1);
+                      }
+                    }}
+                  ></ion-icon>
+                  <p>{quantity}</p>
+                  <ion-icon
+                    name="add-outline"
+                    onClick={() => {
+                      setQuantity(quantity + 1);
+                    }}
+                  ></ion-icon>
+                </QuantityButton>
+                <BuyButton>Comprar</BuyButton>
+              </div>
+            </ProductInfo>
           </>
         ) : (
           <span>NOT FOUND</span>
@@ -41,16 +68,17 @@ export default function ProductPage() {
 const ProductPageMain = styled.main`
   display: flex;
   flex-direction: column;
-  align-items: center;
   background-color: aliceblue;
-  min-height: 100vh;
+  min-height: calc(100vh - 200px);
   span {
     margin-top: 50vh;
     font-size: 30px;
+    font-family: "Montserrat", sans-serif;
   }
   img {
+    margin: 0 auto;
     margin-top: 10px;
-    width: 70vw;
+    width: 50vw;
     max-width: 500px;
   }
   .division {
@@ -61,8 +89,59 @@ const ProductPageMain = styled.main`
     margin-top: 20px;
     margin-bottom: 20px;
   }
+`;
+
+const ProductInfo = styled.div`
+  width: 90vw;
+  margin: 0 auto;
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   h1 {
     font-size: 28px;
     font-weight: bold;
   }
+  p {
+    font-size: 28px;
+    color: rgb(2, 152, 179);
+    font-weight: bold;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  > div {
+    display: flex;
+    margin-bottom: 20px;
+  }
+`;
+
+const QuantityButton = styled.div`
+  width: 130px;
+  height: 50px;
+  border: black 1px solid;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border-radius: 10px;
+  ion-icon {
+    font-size: 30px;
+  }
+  p {
+    color: black;
+    font-size: 16px;
+  }
+`;
+
+const BuyButton = styled.div`
+  width: 200px;
+  height: 50px;
+  background-color: rgb(2, 152, 179);
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border-radius: 30px;
+  margin-left: 20px;
+  font-weight: bold;
+  color: white;
 `;
