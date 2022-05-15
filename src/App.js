d75633ac99eaa5f5bace5e2bf6b4f761f5cf5918
Brenoqn2 <brenoqn2@gmail.com>
2022-05-14 23:16:53 -0300

    first commit: layout + login req

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
+import { useState } from "react";
 import { UserContext } from "./contexts/UserContext.js";
 import "./reset.css";
 
-import SignUp from "./components/sign-up/SignUp";
+import SignUp from "./components/authorization/SignUp";
+import Login from "./components/authorization/Login";
 
 export default function App() {
+  const [token, setToken] = useState();
+  const [username, setUsername] = useState();
   return (
-    <UserContext.Provider value>
+    <UserContext.Provider value={{ token, setToken, username, setUsername }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" />
-          <Route path="/sign-up" element={<SignUp />}/>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </UserContext.Provider>
